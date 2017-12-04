b2d784d0d2973b38f080c43b16d438098a96d6cb
bgalbraith <byron.galbraith@gmail.com>
2017-12-03 23:05:45 -0500

    first pass at pytorch rewrite

--- somnia.py
+++ somnia.py
@@ -4,6 +4,8 @@ import numpy as np
 from PIL import Image
 import pyglet
 from scipy import stats
+import torch
+import torch.nn.functional as F
 
 from ldp8 import LPD8Controller
 
@@ -12,10 +14,16 @@ class SOMNIA(object):
     def __init__(self, source, height=256, width=256, radius=32, alpha=0.1):
         self.height = height
         self.width = width
+        self.k_height = height
+        self.k_width = width
+        self.wrap = (True, True)
 
-        self.som = np.random.randint(0, 255, (height, width, 3), dtype=np.uint8)
+        self.som = 255*torch.rand(1, 3, height, width)
+        self.sample = torch.zeros(1, 3, 1, 1)
+        self.delta = torch.zeros(1, 3, height, width)
+        img = self.som.numpy().squeeze().transpose((1, 2, 0)).flatten()
         self.imdata = pyglet.image.ImageData(width, height, 'RGB',
-                                             array.array('B', self.som.flatten()).tobytes())
+                                             array.array('B', img).tobytes())
         self.radius = radius
         self.alpha = alpha
 
@@ -23,7 +31,8 @@ class SOMNIA(object):
         self.cursor = [0, 0]
         self.cycle = 0
 
-        self.neighborhood, self.weight = self.set_neighborhood_radius()
+        self.weight = torch.zeros(height, width)
+        self.neighborhood, self.template = self.set_neighborhood_radius()
         self.changed = False
 
     def update(self, dt):
@@ -31,10 +40,11 @@ class SOMNIA(object):
             self.neighborhood, self.weight = self.set_neighborhood_radius()
             self.changed = False
 
-        sample = self.get_sample()
-        bmu = self.find_bmu(sample)
-        self.update_neighborhood(sample, bmu)
-        self.imdata.set_data('RGB', self.width*3, array.array('B', self.som.flatten()).tobytes())
+        self.get_sample()
+        bmu = self.find_bmu()
+        self.update_neighborhood(bmu)
+        img = self.som.numpy().squeeze().transpose((1, 2, 0)).flatten()
+        self.imdata.set_data('RGB', self.width*3, array.array('B', img).tobytes())
 
     def get_sample(self):
         pixel = self.source.getpixel(tuple(self.cursor))
@@ -47,48 +57,55 @@ class SOMNIA(object):
             self.cursor = [0, 0]
             self.cycle += 1
 
-        return pixel
+        self.sample[0] = torch.Tensor(pixel)
 
-    def find_bmu(self, sample):
-        dist = np.linalg.norm(self.som - sample, axis=2)
-        idx = np.argmin(dist)
-        return np.array([idx // self.width, idx % self.width]).reshape((2, 1, 1))
+    def find_bmu(self):
+        self.delta = self.sample - self.som
+        dist = -1*torch.norm(self.delta, 2, 1)
+        _, idx = F.max_pool2d(dist, (self.k_height, self.k_width),
+                              return_indices=True)
 
-    def update_neighborhood(self, sample, bmu):
+        idx = idx.data.numpy().squeeze()
+        y = idx // self.width
+        x = idx % self.width
+
+        return torch.from_numpy(np.array([[y], [x]]))
+
+    def update_neighborhood(self, bmu):
         neighbors = self.neighborhood + bmu
-        if False:
-            neighbors[0, neighbors[0] >= self.height] -= self.height
-            neighbors[1, neighbors[1] >= self.width] -= self.width
-        else:
-            neighbors[0, neighbors[0] >= self.height] = self.height - 1
-            neighbors[1, neighbors[1] >= self.width] = self.width - 1
-            neighbors[0, neighbors[0] < 0] = 0
-            neighbors[1, neighbors[1] < 0] = 0
 
-        update_index = [neighbors[0], neighbors[1]]
+        if self.wrap[0]:
+            neighbors[0] %= self.height
+        if self.wrap[1]:
+            neighbors[1] %= self.width
 
-        self.som[update_index] += (self.weight * (sample - self.som[update_index])).astype(np.uint8)
+        self.weight *= 0
+        self.weight[neighbors[0], neighbors[1]] = self.template
+        up = self.weight * self.delta
+        self.som += up
         self.som[self.som < 0] = 0
         self.som[self.som > 255] = 255
 
-    def set_neighborhood_radius(self, mode='rectangle'):
+    def set_neighborhood_radius(self, mode='dog'):
         r = self.radius
         length = r*2+1
-        neighborhood = np.mgrid[0:length, 0:length] - r
-        distance = np.linalg.norm(neighborhood, axis=0)
+        n = np.mgrid[0:length, 0:length].reshape((2, length*length)) - r
+        d = np.linalg.norm(n, axis=0)
 
-        weight = np.zeros_like(distance)
+        weight = np.zeros(len(d))
         if mode == 'rectangle':
-            weight = np.ones_like(distance)
+            weight = np.ones(len(d))
         elif mode == 'gaussian':
-            weight = r * stats.norm.pdf(distance, 0, r / 2)
+            weight = r * stats.norm.pdf(d, 0, r / 2)
         elif mode == 'dog':
-            weight = r * (stats.norm.pdf(distance, 0, r / 2) - 0.5*stats.norm.pdf(distance, 0, r))
+            g1 = stats.norm.pdf(d, 0, r / 2)
+            g2 = 0.5*stats.norm.pdf(d, 0, r)
+            weight = r * (g1 - g2)
 
-        weight[distance > r] = 0
-        weight = self.alpha * weight.reshape((length, length, 1))
+        weight[d > r] = 0
+        weight *= self.alpha
 
-        return neighborhood, weight
+        return torch.from_numpy(n), torch.from_numpy(weight.astype(np.float32))
 
     def save_screenshot(self, value, dt):
         n_pixels = self.cursor[0] + self.source.width*self.cursor[1]
@@ -106,8 +123,8 @@ class SOMNIA(object):
 
 
 if __name__ == '__main__':
-    somnia = SOMNIA('data/nin-seed2.jpg', width=256, height=256, alpha=0.05,
-                    radius=16)
+    somnia = SOMNIA('data/real_nvp.jpeg', width=256, height=256, alpha=0.25,
+                    radius=32)
     controller = LPD8Controller()
     try:
         controller.open()
@@ -117,7 +134,7 @@ if __name__ == '__main__':
     except OSError:
         pass
 
-    window = pyglet.window.Window(somnia.width, somnia.height, vsync=True,
+    window = pyglet.window.Window(somnia.width, somnia.height, vsync=False,
                                   fullscreen=False)
 
     @window.event
@@ -125,5 +142,5 @@ if __name__ == '__main__':
         window.clear()
         somnia.imdata.blit(0, 0, 0)
 
-    pyglet.clock.schedule_interval(somnia.update, 1/30)
+    pyglet.clock.schedule(somnia.update)
     pyglet.app.run()
