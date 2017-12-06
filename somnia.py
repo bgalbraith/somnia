6644a22a3cefd4820428caed6cfcda565a11ea76
bgalbraith <byron.galbraith@gmail.com>
2017-12-05 17:53:21 -0800

    support subsom tiling, changing neighborhood mode, and shuffled input

--- somnia.py
+++ somnia.py
@@ -1,4 +1,5 @@
 import array
+from functools import partial
 
 import numpy as np
 from PIL import Image
@@ -6,19 +7,21 @@ import pyglet
 from scipy import stats
 import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 from ldp8 import LPD8Controller
 
 
 class SOMNIA(object):
-    def __init__(self, source, height=256, width=256, radius=32, alpha=0.1,
-                 wrap=(True, True)):
+    def __init__(self, dataset, height=256, width=256, radius=32, alpha=0.1,
+                 mode='rectangle', wrap=(True, True), tiling=1):
         self.height = height
         self.width = width
-        self.k_height = height
-        self.k_width = width
+        self.tiling = tiling
+        self.k_height = height // self.tiling
+        self.k_width = width // self.tiling
         self.wrap = wrap
+        self.mode = mode
 
         self.som = 255*torch.rand(1, 3, height, width)
         self.delta = torch.zeros(1, 3, height, width)
@@ -28,7 +31,7 @@ class SOMNIA(object):
         self.radius = radius
         self.alpha = alpha
 
-        self.dataset = SOMNIADataset(source)
+        self.dataset = iter(dataset)
 
         self.weight = torch.zeros(height, width)
         self.neighborhood, self.template = self.set_neighborhood_radius()
@@ -36,7 +39,7 @@ class SOMNIA(object):
 
     def update(self, dt):
         if self.changed:
-            self.neighborhood, self.weight = self.set_neighborhood_radius()
+            self.neighborhood, self.template = self.set_neighborhood_radius()
             self.changed = False
 
         sample = self.get_sample()
@@ -58,46 +61,49 @@ class SOMNIA(object):
         y = idx // self.width
         x = idx % self.width
 
-        return torch.from_numpy(np.array([[y], [x]]))
+        return torch.from_numpy(np.array([y.flatten(), x.flatten()]))
 
     def update_neighborhood(self, bmu):
-        neighbors = self.neighborhood + bmu
+        for i in range(bmu.shape[1]):
+            neighbors = self.neighborhood + bmu[:, [i]]
 
-        mask = torch.ones(len(self.template)).type(torch.ByteTensor)
-        if self.wrap[0]:
-            neighbors[0] %= self.height
-        else:
-            mask *= neighbors[0] >= 0
-            mask *= neighbors[0] < self.height
+            mask = torch.ones(len(self.template)).type(torch.ByteTensor)
+            if self.wrap[0]:
+                neighbors[0] %= self.height
+            else:
+                mask *= neighbors[0] >= 0
+                mask *= neighbors[0] < self.height
 
-        if self.wrap[1]:
-            neighbors[1] %= self.width
-        else:
-            mask *= neighbors[1] >= 0
-            mask *= neighbors[1] < self.width
+            if self.wrap[1]:
+                neighbors[1] %= self.width
+            else:
+                mask *= neighbors[1] >= 0
+                mask *= neighbors[1] < self.width
 
-        neighbors = neighbors.masked_select(mask).view(2, -1)
-        template = self.template.masked_select(mask)
-        self.weight *= 0
-        self.weight[neighbors[0], neighbors[1]] = template
-        self.som += self.weight * self.delta
+            neighbors = neighbors.masked_select(mask).view(2, -1)
+            template = self.template.masked_select(mask)
+            self.weight *= 0
+            self.weight[neighbors[0], neighbors[1]] = template
+            self.som += self.weight * self.delta
         self.som[self.som < 0] = 0
         self.som[self.som > 255] = 255
 
-    def set_neighborhood_radius(self, mode='dog'):
+    def set_neighborhood_radius(self):
         r = self.radius
         length = r*2+1
         n = np.mgrid[0:length, 0:length].reshape((2, length*length)) - r
         d = np.linalg.norm(n, axis=0)
 
         weight = np.zeros(len(d))
-        if mode == 'rectangle':
+        if self.mode == 'rectangle':
             weight = np.ones(len(d))
-        elif mode == 'gaussian':
-            weight = r * stats.norm.pdf(d, 0, r / 2)
-        elif mode == 'dog':
-            g1 = stats.norm.pdf(d, 0, r / 2)
-            g2 = 0.5*stats.norm.pdf(d, 0, r)
+        elif self.mode == 'linear':
+            weight = 1 - d / np.max(d)
+        elif self.mode == 'gaussian':
+            weight = r * stats.norm.pdf(d, 0, r / 3)
+        elif self.mode == 'dog':
+            g1 = stats.norm.pdf(d, 0, r / 2.5)
+            g2 = 0.5*stats.norm.pdf(d, 0, r / 1.5)
             weight = r * (g1 - g2)
 
         weight[d > r] = 0
@@ -106,8 +112,7 @@ class SOMNIA(object):
         return torch.from_numpy(n), torch.from_numpy(weight.astype(np.float32))
 
     def save_screenshot(self, value, dt):
-        n_pixels = self.cursor[0] + self.source.width*self.cursor[1]
-        self.imdata.save('somnia_{}_{}.png'.format(self.cycle, n_pixels))
+        self.imdata.save('somnia_{}.png'.format(self.dataset.cursor))
 
     def update_radius(self, value, dt):
         self.radius = int(1.0 * (value+1))
@@ -118,6 +123,11 @@ class SOMNIA(object):
     def update_alpha(self, value, dt):
         self.alpha = value / 127
         self.changed = True
+
+    def change_neighborhood(self, mode, value, dt):
+        self.mode = mode
+        self.changed = True
+
 
 
 class SOMNIADataset(Dataset):
@@ -130,7 +140,7 @@ class SOMNIADataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, item):
-        return self.data[item].view(1, 3, 1, 1)
+        return self.data[item].view(3, 1, 1)
 
     def __next__(self):
         if self.cursor >= len(self):
@@ -141,14 +151,24 @@ class SOMNIADataset(Dataset):
 
 
 if __name__ == '__main__':
-    somnia = SOMNIA('data/real_nvp.jpeg', width=256, height=256, alpha=0.25,
-                    radius=16, wrap=(False, False))
+    dataset = SOMNIADataset('data/real_nvp.jpeg')
+    loader = DataLoader(dataset, shuffle=True)
+    somnia = SOMNIA(loader, width=128, height=128, alpha=0.05, radius=2,
+                    wrap=(False, False), tiling=4, mode='rectangle')
     controller = LPD8Controller()
     try:
         controller.open()
         controller.set_knob_callback(0, somnia.update_radius)
         controller.set_knob_callback(1, somnia.update_alpha)
         controller.set_pad_down_callback(0, somnia.save_screenshot)
+        controller.set_pad_down_callback(3, partial(somnia.change_neighborhood,
+                                                    'rectangle'))
+        controller.set_pad_down_callback(4, partial(somnia.change_neighborhood,
+                                                    'linear'))
+        controller.set_pad_down_callback(5, partial(somnia.change_neighborhood,
+                                                    'gaussian'))
+        controller.set_pad_down_callback(6, partial(somnia.change_neighborhood,
+                                                    'dog'))
     except OSError:
         pass
 
